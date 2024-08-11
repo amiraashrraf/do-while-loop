5108d89bdcdaf4fd033866bb87c0c5db4b46381d
amiraashrraf <168367228+amiraashrraf@users.noreply.github.com>
2024-08-11 22:52:33 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  //condition for looping
+  //the print statement is done once even the condition is false
+  bool isconnected = false;
+  do {
+    print('send notification');
+  } while (isconnected);
+}
